99ed60bbe75672dbaf082f01d1eab782dd902c2e
PatilSri585 <sri.patil321@gmail.com>
2026-01-11 13:54:09 +0530

    Conflict commit on main

--- Demo.py
+++ Demo.py
@@ -5,3 +5,5 @@ print("It demonstrates basic print functionality.") #3rd Commit
 #Test1 branch is carved from 3rd commit
 print("This line is added in Test1 branch.") #Test1 Commit
 print("Another line in Test1 branch.") #Test1 Commit 2
+
+print("conflict commit.") #Conflict commit on main
